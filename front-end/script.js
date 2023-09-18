8c9871aa48bc6b0ad11931f98e26e6907269e594
DavidEvans <david@evanstmd.plus.com>
2023-09-18 15:42:03 +0100

    Continued adding to js

--- front-end/script.js
+++ front-end/script.js
@@ -1,13 +1,14 @@
 const startButton = document.getElementById("start")
 
-console.log(startButton)
+//console.log(startButton)
 
 startButton.addEventListener("click", startGame)
 
 async function startGame(){
+
     const repsData = await fetch(``)
     if (repsData.ok){
         const data = await repsData.json()
-        
+
     }
 }
