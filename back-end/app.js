4f86e290431fb18e346df85fd78e71893cd860a5
Abdulrahman <abdulx9999@gmail.com>
2023-09-18 15:35:49 +0100

    Added Logger to app.js

--- back-end/app.js
+++ back-end/app.js
@@ -1,11 +1,13 @@
 const cors = require("cors");
 const express = require("express");
 const app = express();
+const logger = require("./logger");
 // const port = 3000
 const historicalFigures = require("./historicalFigures.json");
 
 app.use(cors());
 app.use(express.json());
+app.use(logger);
 
 // app.get("/", (req, res) => {
 //     res.send("Hello Figures!");    
