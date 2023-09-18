bf77ef19e4fe27c9d57a6106b1e3da4da024dea8
Abdulrahman <abdulx9999@gmail.com>
2023-09-18 10:32:35 +0100

    API Working 1.0

---
+++ back-end/app.js
@@ -0,0 +1,33 @@
+const cors = require("cors");
+const express = require("express");
+const app = express();
+const port = 3000
+const historicalFigures = require("./historicalFigures.json");
+
+app.use(cors());
+app.use(express.json());
+
+// app.get("/", (req, res) => {
+//     res.send("Hello Figures!");    
+// });
+
+// app.get("/all", (req, res) => {
+//     res.send(historicalFigures);    
+// });
+
+app.get("/", (req, res) => {
+    res.send(historicalFigures[Math.floor(Math.random()*historicalFigures.length)]);  
+});
+
+app.post("/Create", (req, res) => {
+    const newFigure = req.body; 
+    newFigure["id"] = historicalFigures.length;
+    //historicalFigures.push(newFigure)
+    //console.log(newFigure);
+    res.status(201).send(newFigure);
+    
+})
+
+app.listen(port, () => {
+    console.log(`API listening on port ${port}.`);
+})
