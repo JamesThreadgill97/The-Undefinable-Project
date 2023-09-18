1dec58dacb9ac7a52951f10aa2697bc1db7ccc99
DavidEvans <david@evanstmd.plus.com>
2023-09-18 15:58:27 +0100

    Retrieves a random historical figure when start is pressed

--- front-end/script.js
+++ front-end/script.js
@@ -5,10 +5,22 @@ const startButton = document.getElementById("start")
 startButton.addEventListener("click", startGame)
 
 async function startGame(){
-
-    const repsData = await fetch(``)
+try{
+    const repsData = await fetch(`http://localhost:3000/`)
     if (repsData.ok){
         const data = await repsData.json()
+        const hist = {
+            name: data.name,
+            hint1: data.hint1,
+            hint2: data.hint2,
+            funFact: data.funFact
+        }
+        console.log(hist)
 
+    }else{
+        throw "Somethhing has gone wrong with the API request"
     }
+}catch (e){
+    console.log(e)
+}
 }
