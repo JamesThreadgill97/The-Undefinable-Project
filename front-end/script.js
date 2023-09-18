9735414a69d94ba21ad682ef9253cb8f3590cd34
DavidEvans <david@evanstmd.plus.com>
2023-09-18 15:35:48 +0100

    Adding some scripts to front-end

---
+++ front-end/script.js
@@ -0,0 +1,13 @@
+const startButton = document.getElementById("start")
+
+console.log(startButton)
+
+startButton.addEventListener("click", startGame)
+
+async function startGame(){
+    const repsData = await fetch(``)
+    if (repsData.ok){
+        const data = await repsData.json()
+        
+    }
+}
