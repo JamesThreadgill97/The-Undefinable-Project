364ac63760511d54a6390aac8439d6e4bf5689b3
Abdulrahman <abdulx9999@gmail.com>
2023-09-19 11:51:07 +0100

    Added validation to the createNewFigure function

--- front-end/script.js
+++ front-end/script.js
@@ -36,36 +36,42 @@ try{
 }
 
 async function createNewFigure(e) {
-
     e.preventDefault();
-  
-    const data = {
-        name: e.target.name.value,
-        hint1: e.target.hint1.value,
-        hint2: e.target.hint2.value ,
-        funFact: e.target.funFact.value 
 
+    if (e.target.name.value == "" ||e.target.hint1.value == "" ||e.target.hint2.value == "" ||e.target.funFact.value == "" ){
+        alert("Fill out all the boxes.")
     }
-    console.log(data)
-    const options = {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json"
-        },
-        body: JSON.stringify(data)
-    }
-  
-    const response = await fetch("http://localhost:3000/Create", options);
-    console.log(response)
-    if (response.status == 201) {
-      e.target.name.value = ''
-      e.target.hint1.value = ''
-      e.target.hint2.value = ''
-      e.target.funFact.value  = ''
-      alert("Figure added.")
-    }
+
     else{
-      alert("Something is wrong")
-    }
+        
+        const data = {
+            name: e.target.name.value,
+            hint1: e.target.hint1.value,
+            hint2: e.target.hint2.value ,
+            funFact: e.target.funFact.value 
+    
+        }
+        console.log(data)
+        
+        const options = {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(data)
+        }
+    
+        const response = await fetch("https://backendfigures.onrender.com/Create", options);
+        console.log(response)
+        if (response.status == 201) {
+            e.target.name.value = ''
+            e.target.hint1.value = ''
+            e.target.hint2.value = ''
+            e.target.funFact.value  = ''
+            alert("Figure added.")
+            }
+        else{
+            alert("Something is wrong")
+        }
   }
-  
+}  
