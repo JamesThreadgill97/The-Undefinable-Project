466c238d3af0eb4868fdf7072ac7c97afbea9ef8
DavidEvans <david@evanstmd.plus.com>
2023-09-19 13:37:36 +0100

    Hangman is playable - no on-screen keyboard

--- front-end/script.js
+++ front-end/script.js
@@ -4,52 +4,67 @@ const strtbtn = document.getElementById("StartButton")
 const underscores = document.getElementById("HiddenWord")
 const hints = document.getElementById("hint")
 const hintText = document.getElementById("hintText")
-const guessBox = document.getElementById("guessInput")
-const submit = document.getElementById("submitGuess")
+//const guessBox = document.getElementById("guessInput")
+//const submit = document.getElementById("submitGuess")
+const guesses = document.querySelector("#guess-form")
 const createForm =document.querySelector("#create-form");
+const vicLos = document.getElementById("winLose")
+const lettersUsed = document.getElementById("letterBank")
 
+console.log(guesses)
 
+let letter_bank = []
 let noHints = 2
-console.log(hintText)
+let lives = 9
 
 startButton.addEventListener("click", startGame)
 hints.addEventListener("click", provideHint)
-submit.addEventListener("click", submitGuess)
+guesses.addEventListener("submit", submitGuess)
 createForm.addEventListener("submit", createNewFigure);
 
-async function startGame(){
-    
-try{
-    const repsData = await fetch(`https://backendfigures.onrender.com/`)
-    if (repsData.ok){
-        const data = await repsData.json()
-        window.hist = {
-            name: data.name,
-            hint1: data.hint1,
-            hint2: data.hint2,
-            funFact: data.funFact
-        }
-        console.log(hist)
-        strtbtn.style.display = "none";
-        introPara.style.display = "none";
-        hints.style.display = "inline";
-        guessBox.style.display = "inline";
-        submit.style.display = "inline";
-
-        window.word = Array.from(new Array(1), _ => Array(hist["name"].length).fill(" _ "))
-        for (let hs = 0; hs < hist["name"].length; hs++){
-            if (hist["name"][hs] == " "){
-                word[0][hs] = "/"
-            }
-        }
-        underscores.textContent = word[0].join("")
-
-    }else{
-        throw "Something has gone wrong with the API request"
-    }
-}catch (e){
-    console.log(e)
+function youWin(){
+    console.log("You win!")
+    vicLos.textContent = `Congratulations! You win! Did you know...${hist["funFact"]}`
 }
+
+function youLose(){
+    console.log("You lose!")
+    vicLos.textContent = `I'm sorry, you're out of lives! Did you know...${hist["funFact"]}`
+}
+
+async function startGame(){
+    try{
+        const repsData = await fetch(`https://backendfigures.onrender.com/`)
+        if (repsData.ok){
+            const data = await repsData.json()
+            window.hist = {
+                name: data.name,
+                hint1: data.hint1,
+                hint2: data.hint2,
+                funFact: data.funFact
+            }
+            console.log(hist)
+            strtbtn.style.display = "none";
+            introPara.style.display = "none";
+            hints.style.display = "inline";
+            guesses.style.display = "inline";
+            createForm.style.display = "none";
+
+            window.word = Array.from(new Array(1), _ => Array(hist["name"].length).fill(" _ "))
+            for (let hs = 0; hs < hist["name"].length; hs++){
+                if (hist["name"][hs] == " "){
+                    word[0][hs] = "/"
+                }
+            }
+            underscores.textContent = word[0].join("")
+            lettersUsed.textContent = "Letters used: "
+
+        }else{
+            throw "Something has gone wrong with the API request"
+        }
+    }catch (e){
+        console.log(e)
+    }
 }
 
 
@@ -68,13 +83,52 @@ function provideHint(){
 }
 
 function submitGuess(e){
+    
     e.preventDefault();
-    console.log(e.target.guessInput)
     const data = e.target.guessInput.value
     let name = hist["name"]
-    console.log(data)
-    console.log(name)
     e.target.guessInput.value = ""
+    
+    if (letter_bank.includes(data)){
+        alert(`You've already used "${data.toUpperCase()}"!`)
+
+    }else{letter_bank.push(data)
+        lettersUsed.textContent = `Letters used: ${letter_bank.join(", ")}`
+    
+    
+        if (name.toLowerCase().includes(data.toLowerCase())){
+            for(let j = 0; j < name.length; j++){
+                if (name[j].toLowerCase() == data.toLowerCase()){
+                    if (j == 0 || (j > 0 && word[0][j - 1] == "/")){
+                        word[0][j] = data.toUpperCase()
+                    }else{
+                        word[0][j] = data.toLowerCase()
+        
+                    }
+                }
+            }
+            underscores.textContent = word[0].join("")
+            console.log(underscores.textContent)
+            console.log(underscores.textContent.includes(" _ "))
+            if (underscores.textContent.includes("_") == false){
+                youWin()
+            }
+        
+        }else{
+            lives--
+            console.log(`You have ${lives} lives remaining!`)
+            if (lives == 0){
+                youLose()
+                
+            }
+        }
+
+    }
+    
+    
+
+    
+    
 }
 
 async function createNewFigure(e) {
@@ -86,8 +140,8 @@ async function createNewFigure(e) {
         hint1: e.target.hint1.value,
         hint2: e.target.hint2.value ,
         funFact: e.target.funFact.value 
-
     }
+
     console.log(data)
     const options = {
         method: "POST",
@@ -97,7 +151,7 @@ async function createNewFigure(e) {
         body: JSON.stringify(data)
     }
   
-    const response = await fetch("http://localhost:3000/Create", options);
+    const response = await fetch("https://backendfigures.onrender.com/Create", options);
     console.log(response)
     if (response.status == 201) {
       e.target.name.value = ''
