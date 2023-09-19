4d6062281cc9ed7d8649c414b718b0d4d1235bf5
DavidEvans <david@evanstmd.plus.com>
2023-09-19 10:39:46 +0100

    Added hint capability and displaying the underscored word

--- front-end/script.js
+++ front-end/script.js
@@ -1,19 +1,26 @@
 const startButton = document.getElementById("start")
 const introPara = document.getElementById("siteIntro")
 const strtbtn = document.getElementById("StartButton")
+const underscores = document.getElementById("HiddenWord")
+const hints = document.getElementById("hint")
+const hintText = document.getElementById("hintText")
+
+let noHints = 2
+console.log(hintText)
 
 //console.log(introPara)
 //console.log(strtbtn)
 //console.log(startButton)
 
 startButton.addEventListener("click", startGame)
+hints.addEventListener("click", provideHint)
 
 async function startGame(){
 try{
     const repsData = await fetch(`https://backendfigures.onrender.com/`)
     if (repsData.ok){
         const data = await repsData.json()
-        const hist = {
+        window.hist = {
             name: data.name,
             hint1: data.hint1,
             hint2: data.hint2,
@@ -22,6 +29,16 @@ try{
         console.log(hist)
         strtbtn.style.display = "none";
         introPara.style.display = "none";
+        hints.style.display = "inline";
+
+        word = Array.from(new Array(1), _ => Array(hist["name"].length).fill(" _ "))
+        for (let hs = 0; hs < hist["name"].length; hs++){
+            if (hist["name"][hs] == " "){
+                word[0][hs] = "/"
+            }
+        }
+        console.log(word[0])
+        underscores.textContent = word[0].join("")
 
     }else{
         throw "Something has gone wrong with the API request"
@@ -29,4 +46,19 @@ try{
 }catch (e){
     console.log(e)
 }
+}
+
+async function provideHint(){
+    if (noHints > 0){
+        if (noHints == 2){
+            hintText.textContent = hist["hint1"]
+        }else{
+            hintText.textContent = hist["hint2"]
+        }
+        noHints--
+    }else{
+        hintText.textContent = "You're out of hints!"
+    }
+    console.log(hintText.textContent)
+
 }
