f3b0caeba88ef6c72a455bcf4e3b37a11024fcb5
DavidEvans <david@evanstmd.plus.com>
2023-09-19 11:19:11 +0100

    Trying to add the ability to submit guesses

--- front-end/script.js
+++ front-end/script.js
@@ -4,6 +4,8 @@ const strtbtn = document.getElementById("StartButton")
 const underscores = document.getElementById("HiddenWord")
 const hints = document.getElementById("hint")
 const hintText = document.getElementById("hintText")
+const guessBox = document.getElementById("guessInput")
+const submit = document.getElementById("submitGuess")
 
 let noHints = 2
 console.log(hintText)
@@ -14,6 +16,7 @@ console.log(hintText)
 
 startButton.addEventListener("click", startGame)
 hints.addEventListener("click", provideHint)
+submit.addEventListener("click", submitGuess)
 
 async function startGame(){
 try{
@@ -30,14 +33,15 @@ try{
         strtbtn.style.display = "none";
         introPara.style.display = "none";
         hints.style.display = "inline";
+        guessBox.style.display = "inline";
+        submit.style.display = "inline";
 
-        word = Array.from(new Array(1), _ => Array(hist["name"].length).fill(" _ "))
+        window.word = Array.from(new Array(1), _ => Array(hist["name"].length).fill(" _ "))
         for (let hs = 0; hs < hist["name"].length; hs++){
             if (hist["name"][hs] == " "){
                 word[0][hs] = "/"
             }
         }
-        console.log(word[0])
         underscores.textContent = word[0].join("")
 
     }else{
@@ -48,7 +52,7 @@ try{
 }
 }
 
-async function provideHint(){
+function provideHint(){
     if (noHints > 0){
         if (noHints == 2){
             hintText.textContent = hist["hint1"]
@@ -60,5 +64,14 @@ async function provideHint(){
         hintText.textContent = "You're out of hints!"
     }
     console.log(hintText.textContent)
+}
 
+function submitGuess(e){
+    e.preventDefault();
+    console.log(e.target.guessInput)
+    const data = e.target.guessInput.value
+    let name = hist["name"]
+    console.log(data)
+    console.log(name)
+    e.target.guessInput.value = ""
 }
