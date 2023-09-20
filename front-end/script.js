499dfb446e68b645243eecaf738ed392995b951e
Abdulrahman <abdulx9999@gmail.com>
2023-09-20 15:12:11 +0100

    Testing Fix

--- front-end/script.js
+++ front-end/script.js
@@ -13,7 +13,7 @@ const lettersUsed = document.getElementById("letterBank")
 const graphic = document.getElementById("hangmanGraphic")
 const counter = document.getElementById("WinLossCounter")
 const link = document.getElementById("hyperlink")
-const { Console } = require("console")
+//const { Console } = require("console")
 
 console.log(counter)
 
@@ -221,4 +221,4 @@ async function createNewFigure(e) {
   }
 }
 
-module.exports={youWin,youLose,startGame,provideHint,submitGuess,createNewFigure,testEnvironment: 'jsdom'}
+//module.exports={youWin,youLose,startGame,provideHint,submitGuess,createNewFigure,testEnvironment: 'jsdom'}
