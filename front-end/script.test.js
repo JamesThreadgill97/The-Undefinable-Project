e71c73345ff60cc560efb33faed02385cec4b3a4
Abdulrahman <abdulx9999@gmail.com>
2023-09-20 14:32:32 +0100

    Added More Testing

--- front-end/script.test.js
+++ front-end/script.test.js
@@ -1,2 +1,57 @@
 const each = require("jest-each").default;
 const { youWin,youLose,startGame,provideHint,submitGuess,createNewFigure} = require("./script.js");
+
+describe("youWin", () => {
+
+    it("Is a function", () => {
+        expect(youWin).toBeDefined();
+        expect(youWin instanceof Function).toEqual(true);
+    })
+    
+})
+
+describe("youLose", () => {
+
+    it("Is a function", () => {
+        expect(youLose).toBeDefined();
+        expect(youLose instanceof Function).toEqual(true);
+    })
+    
+})
+
+describe("startGame", () => {
+
+    it("Is a function", () => {
+        expect(startGame).toBeDefined();
+        expect(startGame instanceof Function).toEqual(true);
+    })
+    
+})
+
+describe("provideHint", () => {
+
+    it("Is a function", () => {
+        expect(provideHint).toBeDefined();
+        expect(provideHint instanceof Function).toEqual(true);
+    })
+    
+})
+
+describe("submitGuess", () => {
+
+    it("Is a function", () => {
+        expect(submitGuess).toBeDefined();
+        expect(submitGuess instanceof Function).toEqual(true);
+    })
+    
+})
+
+describe("createNewFigure", () => {
+
+    it("Is a function", () => {
+        expect(createNewFigure).toBeDefined();
+        expect(createNewFigure instanceof Function).toEqual(true);
+    })
+    
+})
+
